370b2c56a87c3d7378ba76e9271350e993957598
Chris Brownell <42624201+chrisbrownell@users.noreply.github.com>
2018-09-01 14:17:59 -0400

    Update Lab1-Blink-Dice-External.ino

--- Lab1-Blink-Dice-External.ino
+++ Lab1-Blink-Dice-External.ino
@@ -1,30 +1,16 @@
 /*
-  Blink
+  Blink Dice
 
-  Turns an LED on for one second, then off for one second, repeatedly.
+  This program repeatedly rolls a virtual die, producing a result between 1 and 6. 
+  After a 2 second "roll", the result is communicated via a blinking LED on pin 9.
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
+  This code is based on the public domain Blink program.
   http://www.arduino.cc/en/Tutorial/Blink
 */
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  // initialize pin 9 as an output.
   pinMode(9, OUTPUT);
   Serial.begin(9600);
 }
